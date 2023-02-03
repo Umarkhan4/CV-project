7268d7222737d904d521268bad6265291fee872b
UmarKhan4 <muhammadumarkhan69@gmail.com>
2023-02-03 07:39:09 +0500

    javascript

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function onclickMenu(){
 
     document.getElementById("nav").classList.toggle("change")
 }
+// num generation 
+let valueDisply = document.querySelectorAll(".num");
+let interval = 4000;
+valueDisply.forEach((valueDisply) => {
+let startValue = 0;
+let endValue = parseInt(valueDisply.getAttribute
+   ("data-val"));
+let duration = Math.floor(interval / endValue);
+let counter = setInterval(function(){
+   startValue += 1;
+   valueDisply.textContent = startValue;
+   if (startValue == endValue){
+       clearInterval(counter)
+   }
+},
+duration);
+} );
